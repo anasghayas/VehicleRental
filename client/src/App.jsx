d899b4353ab5c8d3fd4d1bf9f58ed7f845aef303
anasghayas <anasghayas2004@gmail.com>
2026-07-17 18:23:41 +0530

    create and integrate Navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,12 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         {/* Define which component loads for which URL */}
         <Route path="/" element={<Home />} />
