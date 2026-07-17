3446a0001afb4c287ef5de9a137cf20585969bac
anasghayas <anasghayas2004@gmail.com>
2026-07-17 15:40:40 +0530

    create initial express server setup

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config(); 
+
+const app = express();
+
+app.use(cors()); 
+app.use(express.json()); 
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the GoVroom API! The server is running perfectly.');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running beautifully on port ${PORT}`);
+});
